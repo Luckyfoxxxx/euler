0632f3578fcacc27d4f2dc3d39e9edb2c8eb567a
Mikael <secretunicornclub@gmail.com>
2016-06-25 04:43:34 +0200

    First Commit

---
+++ multiples_3_5.js
@@ -0,0 +1,29 @@
+var sum = 0
+
+for(i = 1; i < 1000; i++)
+	if (i % 3 && i % 5 === 0) {
+		sum += i
+	}
+
+		else if (i % 3 === 0) {
+			sum+= i
+		}
+
+		else if (i % 5 === 0) {
+			sum+= i
+		}
+
+		else {
+			sum+= 0
+		}
+
+
+console.log(sum)
+
+
+
+
+
+
+
+
